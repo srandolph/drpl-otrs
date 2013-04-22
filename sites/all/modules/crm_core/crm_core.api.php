a224d56d6b564c7d5f6288b6604045d2d3c62553
Scott Randolph <srandolph@tetrasys.org>
2013-04-21 22:24:30 -0500

    building the upload images

---
+++ sites/all/modules/crm_core/crm_core.api.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * @file
+ * Hooks provided by the CRM Core module.
+ */
+
+/**
+ * Deny or allow access to entity CRUD before any other access check.
+ *
+ * Modules implementing this hook can return FALSE to provide a blanket
+ * prevention for the user to perform the requested operation on the specified
+ * entity. If no modules implementing this hook return FALSE but at least one
+ * returns TRUE, then the operation will be allowed, even for a user without
+ * role based permission to perform the operation.
+ *
+ * If no modules return FALSE but none return TRUE either, normal permission
+ * based checking will apply.
+ *
+ * @param string $op
+ *   The request operation: update, create, or delete.
+ * @param object $entity
+ *   The entity to perform the operation on.
+ * @param object $account
+ *   The user account whose access should be determined.
+ * @param string $entity_type
+ *   The machine-name of the entity type of the given $entity.
+ *
+ * @return bool
+ *   TRUE or FALSE indicating an explicit denial of permission or a grant in the
+ *   presence of no other denials; NULL to not affect the access check at all.
+ */
+function hook_crm_core_entity_access($op, $entity, $account, $entity_type) {
+  // No example.
+}
+
+/**
+ * Use a custom label for a contact of bundle CONTACT_BUNDLE.
+ */
+function crm_core_contact_CONTACT_BUNDLE_label($entity) {
+  // No example.
+}
